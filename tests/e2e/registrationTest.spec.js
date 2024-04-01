9a5c6ea4d4d90f14101c5b814b94611d79da7489
Mario Simon <mario.shimon87@gmail.com>
2024-04-01 19:38:16 +0200

    added page object model to tests

---
+++ tests/e2e/registrationTest.spec.js
@@ -0,0 +1,113 @@
+import {test, expect} from '@playwright/test';
+import { HomePage } from './pages/HomePage';
+import { AccountSettings } from './pages/AccountSettingsPage';
+
+test.beforeEach(async ({page}) => {
+    await page.goto('/');
+});
+
+test('Verify all registration page elements are visible', async ({page}) => {
+    
+    const homePage = new HomePage(page);
+
+    await homePage.signUpNowLink.click();
+    await expect.soft(homePage.pageLogo).toBeVisible();
+    await expect.soft(homePage.signInBtn).toBeVisible();
+    await expect.soft(homePage.fullNameInputField).toBeVisible();
+    await expect.soft(homePage.emailInputField).toBeVisible();
+    await expect.soft(homePage.passwordInputField).toBeVisible();
+    await expect.soft(homePage.continueRegistrationBtn).toBeVisible();
+    await expect.soft(homePage.fullPage).toHaveScreenshot('registerPage.png');
+    await homePage.fullNameInputField.fill('John Doe');
+    await homePage.emailInputField.fill('johnDoe@gmail.com');
+    await homePage.passwordInputField.fill('password.123');
+    await homePage.continueRegistrationBtn.click();
+    await expect.soft(homePage.iManageDropdown).toBeVisible();
+    await expect.soft(homePage.accommodationNumberDropdown).toBeVisible();
+    await expect.soft(homePage.countrySelectDropdown).toBeVisible();
+    await expect.soft(homePage.phoneInputField).toBeVisible();
+    await expect.soft(homePage.acceptTermsCheckBox).toBeVisible();
+    await expect.soft(homePage.createAccountBtn).toBeVisible();
+    await expect.soft(homePage.fullPage).toHaveScreenshot('registration2ndStepPage.png');
+
+});
+
+test('Verify all validation messages are displayed', async ({page}) => {
+
+    const homePage = new HomePage(page);
+
+    await homePage.signUpNowLink.click();
+    await homePage.fullNameInputField.fill('John Doe');
+    await homePage.emailInputField.fill('invalidAddress');
+    await homePage.passwordInputField.fill('short');
+    await homePage.continueRegistrationBtn.click();
+    await homePage.iManageSelect('Hotelom');
+    await homePage.accommodationUnitNumberSelect('1-5');
+    await homePage.countrySelect('Croatia');
+    await homePage.phoneInputField.fill('123456789');
+    await homePage.acceptTermsCheckBox.click();
+    await expect.soft(homePage.fullPage).toHaveScreenshot('registration2ndStepPage2.png',{ maxDiffPixelRatio: 0.01 });
+    await homePage.createAccountBtn.click();
+    await expect.soft(page.getByText('Unešena email adresa nije ispravna')).toBeVisible();
+    await expect.soft(page.getByText('Lozinka je prekratka.')).toBeVisible();
+    await expect.soft(homePage.fullPage).toHaveScreenshot('invalidInputValidation.png');
+    await homePage.emailInputField.fill('mario.shimon87@gmail.com');
+    await homePage.passwordInputField.fill('long.123');
+    await homePage.continueRegistrationBtn.click();
+    await homePage.acceptTermsCheckBox.click();
+    await homePage.createAccountBtn.click();
+    await expect(homePage.fullPage).toHaveScreenshot('existingUserValidation.png');
+
+});
+
+test('Verify user data is displayed correctly after registration and login', async ({page}) => {
+
+    const homePage = new HomePage(page);
+    const accountSettingsPage = new AccountSettings(page);
+    const generateRandomString = function() {
+        return Math.random().toString(16).substring(2, 16);
+    };
+    const randomEmail = `email${generateRandomString()}@gmail.com`;
+
+    await homePage.signUpNowLink.click();
+    await homePage.fullNameInputField.fill('John Doe');
+    await homePage.emailInputField.fill(randomEmail);
+    await homePage.passwordInputField.fill('long.123');
+    await homePage.continueRegistrationBtn.click();
+    await homePage.iManageSelect('Hotelom');
+    await homePage.accommodationUnitNumberSelect('1-5');
+    await homePage.countrySelect('Croatia');
+    await homePage.phoneInputField.fill('123456789');
+    await homePage.acceptTermsCheckBox.click();
+    await homePage.createAccountBtn.click();
+    await accountSettingsPage.userMenu.click();
+    await accountSettingsPage.accountSettingsLink.click();
+    await expect.soft(accountSettingsPage.firstNameInputField).toHaveValue('John');
+    await expect.soft(accountSettingsPage.lastNameInputField).toHaveValue('Doe');
+    await expect.soft(accountSettingsPage.stateSelectDropdown).toContainText('Hrvatska');
+    await expect.soft(accountSettingsPage.phoneInputField).toHaveValue('+385123456789');
+    await expect(accountSettingsPage.accountSettingsPage).toHaveScreenshot('newRegistrationAccountSettingsPage.png');
+
+});
+
+test('Page language converter functionality tets', async ({page}) => {
+
+    const homePage = new HomePage(page);
+
+    await homePage.signUpNowLink.click();
+    await homePage.languageConverter.click();
+    await expect.soft(page.getByText('Already a member? Sign in')).toBeVisible();
+    await expect.soft(page.getByRole('heading', { name: 'Start your free trial' })).toBeVisible();
+    await expect.soft(page.getByText('Thanks to Rentlio mobile app, we have full control over reservations, rates, and guests at any given moment.')).toBeVisible();
+    await expect.soft(homePage.fullPage).toHaveScreenshot('registerPageEnglish.png');
+    await homePage.fullNameInputField.fill('John Doe');
+    await homePage.emailInputField.fill('random@gmail.com');
+    await homePage.passwordInputField.fill('random.123');
+    await homePage.continueRegistrationBtn.click();
+    await expect(page.getByRole('heading', { name: 'Tell us more about yourself' })).toBeVisible();
+    await homePage.iManageDropdown.click();
+    await expect(page.getByText('Hotel Vacation Rental Hostel Multi Properties Other Property Type')).toBeVisible();
+    await homePage.iManageDropdown.click();
+    await expect.soft(homePage.fullPage).toHaveScreenshot('registerPage2ndStepEnglish.png');
+
+});
---
+++ tests/e2e/userDataUpdateTest.spec.js
@@ -0,0 +1,32 @@
+import {test, expect} from '@playwright/test';
+import { AccountSettings } from './pages/AccountSettingsPage';
+
+test.beforeEach(async ({page}) => {
+    const accountSettingsPage = new AccountSettings(page);
+    await page.goto('/');
+    await accountSettingsPage.logIn('mario.shimon87@gmail.com', 'Zeus.123');
+});
+
+test('Korisnicki podaci update test', async ({page}) => {
+
+    const accountSettingsPage = new AccountSettings(page);
+
+    test.setTimeout(60000);
+    await accountSettingsPage.userMenu.click({timeout: 40000});
+    await accountSettingsPage.accountSettingsLink.click();
+    await accountSettingsPage.ownerDataUpdate('John', 'Doe', 'another address', '10000', 'Zagreb', 'Hong Kong', '9876543210', '654321');
+    await accountSettingsPage.companyDataUpdate('update poduzece', 'update adressa', '10000', 'Zagreb', 'Haiti','654321');
+    await accountSettingsPage.applicationSettingsUpdate('English', 'Europe/Vatican', '1,234.56');
+    await accountSettingsPage.saveChangesBtn.click();
+    await accountSettingsPage.successfulUpdateMessage.waitFor({state:'visible',timeout:2000});
+    await accountSettingsPage.successfulUpdateMessage.waitFor({state:'hidden',timeout:20000});
+    await expect.soft(accountSettingsPage.accountSettingsLeftConatiner).toHaveScreenshot('korisnickiPodaciUpdate.png');
+    await accountSettingsPage.ownerDataUpdate('Mario', 'Šimon', 'editAddress', '31000', 'Osijek', 'Croatia (Hrvatska)', '0123456789', '123456');
+    await accountSettingsPage.companyDataUpdate('test poduzece', 'test address', '31000', 'Osijek', 'Greenland','123456');
+    await accountSettingsPage.applicationSettingsUpdate('Croatian', 'Europe/Zagreb', '1.234,56');
+    await accountSettingsPage.saveChangesBtn.click();
+    await accountSettingsPage.successfulUpdateMessage.waitFor({state:'visible',timeout:2000});
+    await accountSettingsPage.successfulUpdateMessage.waitFor({state:'hidden',timeout:20000});
+    await expect(accountSettingsPage.accountSettingsLeftConatiner).toHaveScreenshot('korisnickiPodaci.png');
+
+});
